8640c4921afb2e5d0388883e5ab880cdddafa0d1
Lamisa_Ritu <lamisaferdous19@gmail.com>
2022-04-11 02:55:10 +0600

    app installed in settings & models are registered in admin

--- CSE303_Project/settings.py
+++ CSE303_Project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rcras_app.apps.RcrasAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
