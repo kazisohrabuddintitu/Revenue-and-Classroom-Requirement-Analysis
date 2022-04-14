4796528722bd0da77d93185a91f27532e8758f81
Lamisa_Ritu <lamisaferdous19@gmail.com>
2022-04-15 02:15:43 +0600

    app authentication done

--- rcras_app/urls.py
+++ rcras_app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.index,name='index'),
+    path('', views.index, name='index'),
+    path('login', views.loginUser, name='login'),
+    path('logout', views.logoutUser, name='logout'),
     path('revenue/', views.revenue,name='revenue'),
     path('about/', views.about,name='about')
 ]
--- rcras_app/views.py
+++ rcras_app/views.py
@@ -1,9 +1,38 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout, login
 
+# superuser: admin; password: admin
+# user: Lamisa; password: lami$$0000
 
 # Create your views here.
 def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login")
     return render(request,'index.html')
+
+def loginUser(request):
+    if request.method=="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        #check if the user has entered correct credentitals
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            #A backend authenticated the credentials
+            login(request, user)
+            return redirect("/")
+        else:
+            #No backend authenticated the credentials
+            return render(request, 'login.html')
+
+    return render(request,'login.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect("/login")
     
 def revenue(request):
     return render(request,'revenue.html')
