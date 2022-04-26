aecaf4b2419dfc14d116e54824ac47553fa08454
Kazi Sohrab Uddin <kazisohrab73@gmail.com>
2022-04-26 13:25:55 +0600

    Update urls.py

--- DATABASE/RCRAS/urls.py
+++ DATABASE/RCRAS/urls.py
@@ -5,8 +5,13 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.loginUser, name='login'),
     path('logout', views.logoutUser, name='logout'),
-    path('revenue/', views.revenue,name='revenue'),
     path('about/', views.about,name='about'),
+    
+    
     path('view_enrolment_course_school',views.enrollment_wise_course_school,name='view_enrolment_course_school'),
-    #path('classSizeDistribution', views.ClassSizeDistributionView, name='classSizeDistribution'),
+    path('view_classroom_requirement_course_offer',views.view_classroom_requirement_course_offer,name='view_classroom_requirement_course_offer'),
+    path('view_availabilityvscourse_offer',
+         views.view_availabilityvscourse_offer, name='view_availabilityvscourse_offer'),
+    path('view_revenue_of_iub',
+         views.view_revenue_of_iub, name='view_revenue_of_iub'),
 ]
