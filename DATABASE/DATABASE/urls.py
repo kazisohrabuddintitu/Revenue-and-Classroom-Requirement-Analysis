a30c23047e3b5daa6bf5985ff531ae4c5a3772bd
Kazi Sohrab Uddin <kazisohrab73@gmail.com>
2022-04-26 13:24:35 +0600

    Update urls.py

--- DATABASE/DATABASE/urls.py
+++ DATABASE/DATABASE/urls.py
@@ -43,9 +43,15 @@ urlpatterns = [
      path('', views.loginUser, name='login'),
      path('login/', views.loginUser, name='login'),
     path('logout', views.logoutUser, name='logout'),
-    path('revenue/', views.revenue,name='revenue'),
     path('about/', views.about,name='about'),
-     path('view_enrolment_course_school',
+    
+    
+    path('view_enrolment_course_school',
          views.view_enrolment_course_school, name='view_enrolment_course_school'),
-    #path('view_enrolment_course_school',views.enrollment_wise_course_school,name='view_enrolment_course_school'),
+    path('view_classroom_requirement_course_offer',
+         views.view_classroom_requirement_course_offer, name='view_classroom_requirement_course_offer'),
+    path('view_availabilityvscourse_offer',
+         views.view_availabilityvscourse_offer, name='view_availabilityvscourse_offer'),
+    path('view_revenue_of_iub',
+         views.view_revenue_of_iub, name='view_revenue_of_iub'),
 ]
